b5c6004ca7e83bcc171a702244ef27d63fad4dc6
Rehan Raza <rehan.raza87@gmail.com>
2020-11-23 00:16:08 +0600

    Backend api

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/verify', function(req, res, next) {
+  const code  = req.body.code;
+  if(!code) {
+    res.status(400).send({success: false, message: 'Required param missing'})
+  } else {
+    const digits = code.split('');
+    if(digits.length<6 || digits.pop() ==7) {
+      res.status(400).send({success: false, message: 'Invalid code'});
+    } else {
+      res.send({success: true, message: 'Code verified'});
+    }
+  }
+});
+
+module.exports = router;
